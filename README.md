42b6b21a33ee6bd05466c5d974e0b95769308cff
Tobias Kriebisch <tobias.kriebisch@tecbeast.gmbh>
2022-01-04 10:44:44 +0100

    refactor so no clone is required

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cpu Frequency Scanner
+
+Parse /proc/cpuinfo to get the current cpu frequencies of all your cores. Echos the mean of all
+frequencies. This can be used in a status bar like polybar.
+
+## Build
+
+cargo build --relase 
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 fn main() {
-    let f = include_str!("/proc/cpuinfo");
+    let info = include_str!("/proc/cpuinfo");
 
-    let frequencies = f.lines().filter(|line| {
+    let mut count: u64 = 0;
+    let mut sum: f64 = 0.0;
+
+    info.lines().filter(|line| {
         line.contains("MHz")
-    }).map(|line| {
-        line.trim_start_matches("cpu MHz\t\t: ")
+    }).for_each(|line| {
+        sum += line.trim_start_matches("cpu MHz\t\t: ")
             .parse::<f64>()
-            .expect("Could not parse frequency from cpuinfo")
+            .expect("Could not parse frequency from cpuinfo");
+        count += 1;
     });
 
-    let sum: f64 = frequencies.clone().sum();
-    let count = frequencies.count() as f64;
-    let average: u64 = (sum / count) as u64;
+    let average: u64 = sum as u64 / count;
 
     println!("{:?}", average);
 }
